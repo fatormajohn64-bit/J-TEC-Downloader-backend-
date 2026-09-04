d9cb034e965c556db27be1742837997a45a1c93d
Johnny-Tech <fatormajohn64@gmail.com>
2026-09-04 19:35:26 +0000

    Delete app directory

--- app/utils/cleanup.py
+++
@@ -1,8 +0,0 @@
-import os
-import shutil
-
-def remove_temp_folder(folder_path: str):
-    """Deletes temporary folder and contained media after client download finishes."""
-    if os.path.exists(folder_path):
-        shutil.rmtree(folder_path, ignore_errors=True)
-      
